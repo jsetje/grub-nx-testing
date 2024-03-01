rhboot/grub2

nx: set attrs in our kernel loaders
For NX, our kernel loaders need to set write and execute page
permissions on allocated pages and the stack.

This patch adds those calls.

Signed-off-by: Peter Jones <pjones@redhat.com>

grub-core/loader/efi/chainloader.c
@@ -1031,6 +1031,17 @@ grub_cmd_chainloader (grub_command_t cmd __attribute__ ((unused)),
       goto fail;
     }
 
+  /*
+   * The OS kernel is going to set its own permissions when it takes over
+   * paging a few million instructions from now, and load_image() will set up
+   * anything that's needed based on the section headers, so there's no point
+   * in doing anything but clearing the protection bits here.
+   */
+  grub_dprintf("nx", "setting attributes for %p (%lu bytes) to %llx\n",
+	       (void *)(grub_addr_t)address, fsize, 0llu);
+  grub_update_mem_attrs (address, fsize,
+			 GRUB_MEM_ATTR_R|GRUB_MEM_ATTR_W|GRUB_MEM_ATTR_X, 0);
+
 #if defined (__i386__) || defined (__x86_64__)
   if (fsize >= (grub_ssize_t) sizeof (struct grub_macho_fat_header))
     {

jsetje/grub-nx-testing

[rharwood: fix stack_attrs undefined, fix aarch64 callsites]
Signed-off-by: Robbie Harwood <rharwood@redhat.com>
(cherry picked from commit a9d40561377418baa1fda22fdbe06e2b3e35cd94)
Signed-off-by: Jan Setje-Eilers <jan.setjeeilers@oracle.com>
 Conflicts:
	grub-core/kern/efi/mm.c			(fixup)
	grub-core/kern/loongarch64/cache_flush.S
	grub-core/loader/arm64/linux.c
	grub-core/loader/arm64/xen_boot.c
	grub-core/loader/efi/chainloader.c
	grub-core/loader/efi/linux.c		(fixup)
	grub-core/loader/i386/efi/linux.c
	include/grub/efi/efi.h			(fixup)

grub-core/loader/efi/chainloader.c
@@ -307,6 +307,17 @@ grub_cmd_chainloader (grub_command_t cmd __attribute__ ((unused)),
       goto fail;
     }
 
+  /*
+   * The OS kernel is going to set its own permissions when it takes over
+   * paging a few million instructions from now, and load_image() will set up
+   * anything that's needed based on the section headers, so there's no point
+   * in doing anything but clearing the protection bits here.
+   */
+  grub_dprintf("nx", "setting attributes for %p (%lu bytes) to %llx\n",
+	       (void *)(grub_addr_t)address, size, 0llu);
+  grub_update_mem_attrs (address, size,
+			 GRUB_MEM_ATTR_R|GRUB_MEM_ATTR_W|GRUB_MEM_ATTR_X, 0);
+
 #if defined (__i386__) || defined (__x86_64__)
   if (size >= (grub_ssize_t) sizeof (struct grub_macho_fat_header))
     {